30ea8512491d9a6339bcc6e6cccb02cd64149b5c
Daniel Tschan <tschan@puzzle.ch>
2015-10-13 13:46:38 +0200

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM registry.access.redhat.com/openshift3/ose-docker-builder:v3.0.2.0
+MAINTAINER Daniel Tschan <tschan@puzzle.ch>
+
+RUN yum -y --disablerepo=* --enablerepo=rhel-7-server-rpms --enablerepo=rhel-7-server-extras-rpms --enablerepo=rhel-7-server-optional-rpms install docker; yum clean all
+
+ADD ./build.sh /tmp/build.sh
+
+ENTRYPOINT ["/bin/sh", "-c"]
+CMD ["/tmp/build.sh"]
---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -o pipefail
+IFS=$'\n\t'
+
+DOCKER_SOCKET=/var/run/docker.sock
+
+if [ ! -e "${DOCKER_SOCKET}" ]; then
+  echo "Docker socket missing at ${DOCKER_SOCKET}"
+  exit 1
+fi
+
+if [ -n "${OUTPUT_IMAGE}" ]; then
+  TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
+fi
+
+BUILD_DIR=$(mktemp --directory --suffix=docker-build)
+pushd "${BUILD_DIR}"
+
+if [ -e /etc/secret-volume/.netrc ]; then
+  CURL_OPTS="--netrc-file=/etc/secret-volume/.netrc"
+else
+  CURL_OPTS=""
+fi
+
+curl ${CURL_OPTS} ${DOCKERFILE_URL} -O
+curl ${CURL_OPTS} ${ARTIFACT_URL} -O
+
+popd
+docker build --rm -t "${TAG}" "${BUILD_DIR}"
+
+if [ -n "${OUTPUT_IMAGE}" ] || [ -s "/root/.dockercfg" ]; then
+  docker push "${TAG}"
+fi
