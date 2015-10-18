2e7232365fbbc314e1b23cea89a5b226bb73a46b
Daniel Tschan <tschan@puzzle.ch>
2015-10-18 20:16:10 +0200

    Added support for build tags.

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ if [ -n "${OUTPUT_IMAGE}" ] || [ -s "/root/.dockercfg" ]; then
   docker push "${TAG}"
 
   if [ -e ${BUILD_DIR}/.build_tag ]; then
-    BUILD_TAG=`cat ${BUILD_DIR}/.build_tag`
+    BUILD_TAG="${OUTPUT_REGISTRY}/"`cat ${BUILD_DIR}/.build_tag`
     docker tag "${TAG}" "${BUILD_TAG}"
     docker push "${BUILD_TAG}"
   fi
